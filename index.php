72bb2c3c67573f66a71f9fdbc969657fdef8947b
ZIYAD <ziyad.zerrari@gmail.com>
2025-12-30 12:09:49 +0100

    fix

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ if (isset($_POST['submit'])) {
 <head>
     <title>E-Voting Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/evoting/assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body class="login-page">
 
